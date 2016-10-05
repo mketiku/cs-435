d85bbea9f9186343b8692ba60d7c007b10bd02ca
Michael Ketiku <mketiku@gmail.com>
2016-10-05 15:26:27 -0400

    init smtp server

---
+++ tcp_sender.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+import socket
+import sys
+
+
+def main(argv):
+    port = argv[0]
+
+    #create an INET, STREAMing socket
+    s = socket.socket(
+    socket.AF_INET, socket.SOCK_STREAM)
+    # now bind the server to the port
+    s.bind(('', port))
+    s.listen(1)
+    print('Listening for messages')
+    while 1:
+        conn, addr = s.accept()
+        message = conn.recv(1024)
+        response = message.decode('UTF-8').upper()
+        conn.send(bytes(response, 'UTF-8'))
+        conn.close()
+
+    # signal our intent to close the socket and then close it
+    # the shutdown step is _essentially_ optional
+    s.shutdown(socket.SHUT_RDWR)
+    s.close()
+
+
+if __name__ == "__main__":
+    # pass arguments if any exist, otherwise send some defaults (not complete)
+    if len(sys.argv) > 1:
+        main(sys.argv[1:])
+    else:
+        main( [43500] )
